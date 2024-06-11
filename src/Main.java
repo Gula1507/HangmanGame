e7eb7a05c89d6a7c5dca550615ad00802e47d03d
Aygul Fortelka <gulya@gmx.net>
2024-06-11 22:15:23 +0200

    refactor: moved methods from Main class to Start class

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,9 @@
 public class Main {
-    public static String filename;
-    public static boolean isEnglishWord;
 
     public static void main(String[] args) {
-        if (Start.askToStartTheGame().equals("Y")) {
-            if (Start.selectLanguageRusOrEng().equals("ENG")) {
-                filename = "resources/words.txt";
-                isEnglishWord = true;
-            } else {
-                filename = "resources/russian_words.txt";
-                isEnglishWord = false;
-            }
-        }
-        Game game = new Game(filename, isEnglishWord);
-        game.startGameLoop();
+        Start start=new Start();
+        start.execute();
     }
+
+
 }
-
-
--- src/Start.java
+++ src/Start.java
@@ -2,6 +2,41 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Start {
+    public static String filename;
+    public static boolean isEnglishWord;
+
+    public void execute() {
+        if (askToStartTheGame().equals("Y")) {
+            if (Start.selectLanguageRusOrEng().equals("ENG")) {
+                filename = "resources/words.txt";
+                isEnglishWord = true;
+            } else {
+                filename = "resources/russian_words.txt";
+                isEnglishWord = false;
+            }
+            Game game = new Game(filename, isEnglishWord);
+            game.startGameLoop();
+        } else {
+            System.out.println("Bye! See you the next time");
+            System.exit(0);
+        }
+
+    }
+
+    public static String askToStartTheGame() {
+        String questionToStart = "Do you want to start the game (Y/N)?";
+        String questionToRepeat = "Please consider that you tipped 'Y' or 'N'";
+        List<String> keys = List.of("Y", "N");
+        return askQuestionWithAnswerKeys(questionToStart, questionToRepeat, keys);
+
+    }
+
+    public static String selectLanguageRusOrEng() {
+        return askQuestionWithAnswerKeys("Would you like to guess English or Russian words (ENG/RUS)?",
+                "Please consider that you tipped 'ENG' or 'RUS'", List.of("ENG", "RUS"));
+
+    }
+
     public static String askQuestionWithAnswerKeys(String questionToStart, String questionToRepeat, List<String> keys) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -15,16 +50,6 @@ public class Start {
             System.out.println(questionToRepeat);
         }
     }
-    public static String askToStartTheGame() {
-        return askQuestionWithAnswerKeys("Do you want to start the game (Y/N)?",
-                "Please consider that you tipped 'Y' or 'N'", List.of("Y", "N")
 
-        );
-    }
 
-    public static String selectLanguageRusOrEng() {
-        return askQuestionWithAnswerKeys("Would you like to guess English or Russian words (ENG/RUS)?",
-                "Please consider that you tipped 'ENG' or 'RUS'", List.of("ENG", "RUS")
-        );
-    }
 }
