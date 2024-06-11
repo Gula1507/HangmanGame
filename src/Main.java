04a58bbcbda8c26cc298214886ff1098645a6afc
Aygul Fortelka <gulya@gmx.net>
2024-06-11 20:45:09 +0200

    actualised Readme file

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static String filename;
     public static boolean isEnglishWord;
+
     public static void main(String[] args) {
         if (Start.askToStartTheGame().equals("Y")) {
             if (Start.selectLanguageRusOrEng().equals("ENG")) {
-                filename = "resources/words.txt"; isEnglishWord=true;
+                filename = "resources/words.txt";
+                isEnglishWord = true;
             } else {
-                filename = "resources/russian_words.txt";isEnglishWord=false;
+                filename = "resources/russian_words.txt";
+                isEnglishWord = false;
             }
         }
         Game game = new Game(filename, isEnglishWord);
